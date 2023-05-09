aa033a323169ff5b1f7c7bd9feb1f9615ad29ebd
Brisco15 <47005692+Brisco15@users.noreply.github.com>
2023-05-09 08:28:52 +0200

    created Homepage , add Button to create a room

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Router from 'next/router'
 
 export default function Home() {
   return (
@@ -9,7 +10,17 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1>Welcome!</h1>
+      <h1 className='mt-20 text-center text-3xl uppercase text-blue-700'>
+        Videoconf
+      </h1>
+      <button 
+        className='block mx-auto bg-blue-500 text-white p-3 rounded-2xl mt-20 text-2xl hover:bg-blue-300'
+        onClick={()=> {
+          Router.push(`/room/${crypto.randomUUID().split('-')[0]}`)
+        }}
+        >
+        Create a new chat room
+      </button>
     </div>
   )
 }
