5b982da052b0857d42a1bc34b345619e3820bed2
Brisco15 <47005692+Brisco15@users.noreply.github.com>
2023-08-08 15:05:45 +0200

    created Room page and Get the Room id

--- pages/room/[id].js
+++ pages/room/[id].js
@@ -1,7 +1,30 @@
+import Script from "next/script"
+import { useRouter } from 'next/router'
+
 export default function Room() {
+  const router = useRouter()
+  const { id } = router.query 
     return (
-      <h1 className='mt-20 text-center text-3xl uppercase text-blue-500'>
+      <>
+        <Script
+        src='https://unpkg.com/peerjs@1.4.5/dist/peerjs.min.js'
+        onLoad={() => {
+          console.log(Peer)
+          
+        }}
+        />
+        <h1 className='mt-20 text-center text-3xl uppercase text-blue-500'>
         Room
-      </h1>
+        </h1>
+      </>
+      
     )
+}
+
+export async function getServerSideProps(context){
+  //we need this or the router query data is not available client-side
+  //se https;//nextjs.docs/api-reference/next/router#router-object
+  return {
+    props: {},
   }
+}
